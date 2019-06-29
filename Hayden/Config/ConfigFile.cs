62455565b68afc786fd1b3a26031e39869e719a8
Bepis <36346617+bbepis@users.noreply.github.com>
2019-06-29 21:55:24 +1000

    Basic configuration file support

---
+++ Hayden/Config/ConfigFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayden
+{
+	public class ConfigFile
+	{
+		public dynamic Backend { get; set; }
+
+		public dynamic Source { get; set; }
+	}
+}
--- Hayden/Program.cs
+++ Hayden/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Hayden.Consumers;
+using Hayden.Contract;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Hayden
 {
@@ -9,24 +14,40 @@ namespace Hayden
 	{
 		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hayden v0.0.0");
+			Console.WriteLine("Hayden v0.1.0");
 			Console.WriteLine("By Bepis");
 
-			if (args.Length < 2)
+			if (args.Length != 1)
 			{
-				Console.WriteLine("Usage: hayden <board> <download directory location>");
+				Console.WriteLine("Usage: hayden <config file location>");
 				return;
+			}
+
+			var configFile = JsonConvert.DeserializeObject<ConfigFile>(File.ReadAllText(args[0]));
+
+			IThreadConsumer consumer;
+
+			switch ((string)configFile.Backend.type)
+			{
+				case "Asagi":
+					var connection = new MySqlConnection((string)configFile.Backend.connectionString);
+					await connection.OpenAsync();
+
+					consumer = new AsagiThreadConsumer(connection, (string)configFile.Backend.downloadLocation);
+					break;
+
+				default:
+					throw new ArgumentException($"Unknown backend type {(string)configFile.Backend.type}");
 			}
 
 			Log("Initialized.");
 
-			string board = args[0];
-			string downloadDir = args[1];
+			string board = ((JArray)configFile.Source.boards)[0].Value<string>();
 
-			Log($"Downloading from board /{board}/ to directory {downloadDir}");
+			//Log($"Downloading from board /{board}/ to directory {downloadDir}");
 			Log("Press Q to stop archival.");
-
-			var boardArchiver = new BoardArchiver(board, new FilesystemThreadConsumer(downloadDir));
+			
+			var boardArchiver = new BoardArchiver(board, consumer);
 
 			var tokenSource = new CancellationTokenSource();
 
