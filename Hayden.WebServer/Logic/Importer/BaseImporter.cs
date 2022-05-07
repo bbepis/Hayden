8d03fe50ff5bed7d07c1bb9eef62f01bab77c05b
Bepis <36346617+bbepis@users.noreply.github.com>
2022-05-08 00:49:45 +1000

    Add preliminary importer code

---
+++ Hayden.WebServer/Logic/Importer/BaseImporter.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Hayden.Contract;
+using Hayden.WebServer.Controllers;
+using Hayden.WebServer.DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Hayden.WebServer.Logic
+{
+	public abstract class BaseImporter<TThread, TPost, TPostFile> where TThread : IThread<TPost> where TPost : IPost
+	{
+		public async Task Import(string searchDir, HaydenDbContext dbContext, IOptions<Config> config, IProgress<(float, FormattedString)> progress)
+		{
+			float progressPercentage = 0;
+			FormattedString progressString = "Initializing";
+
+			void reportProgress()
+			{
+				progress.Report((progressPercentage, progressString));
+			}
+
+			reportProgress();
+
+			//const string searchDir = "G:\\utg-archive\\anoncafe";
+			//const string searchDir = "G:\\utg-archive\\tvch";
+
+			foreach (var subfolder in Directory.EnumerateDirectories(searchDir, "*", SearchOption.TopDirectoryOnly))
+			{
+				string board = Path.GetFileName(subfolder);
+
+				if (board == "hayden" || board == "server" || board == "temp")
+					continue;
+
+				static T ReadJson<T>(string filename)
+				{
+					using StreamReader streamReader = new StreamReader(File.OpenRead(filename));
+					using JsonReader reader = new JsonTextReader(streamReader);
+
+					return JToken.Load(reader).ToObject<T>();
+				}
+
+				var boardObject = await dbContext.Boards.FirstOrDefaultAsync(x => x.ShortName == board);
+
+				if (boardObject == null)
+				{
+					boardObject = new DBBoard
+					{
+						ShortName = board,
+						LongName = board,
+						Category = "tob",
+						IsNSFW = true
+					};
+
+					dbContext.Add(boardObject);
+					await dbContext.SaveChangesAsync();
+				}
+
+				var baseDirectory = Path.Combine(config.Value.FileLocation, board);
+				Directory.CreateDirectory(Path.Combine(baseDirectory, "image"));
+				Directory.CreateDirectory(Path.Combine(baseDirectory, "thumb"));
+				Directory.CreateDirectory(Path.Combine(baseDirectory, "thread"));
+
+				int totalCount = Directory.EnumerateFiles(subfolder, "thread.json", SearchOption.AllDirectories).Count();
+				int currentCount = 0;
+
+				progressString = (FormattableString)$"Processing threads ({new Box<int>(currentCount)} / {totalCount}) (post {new Box<int>(0)} / {new Box<int>(0)})";
+
+				foreach (var jsonFile in Directory.EnumerateFiles(subfolder, "thread.json", SearchOption.AllDirectories))
+				{
+					progressString.SetBox(0, ++currentCount);
+					progressPercentage = currentCount / (float)totalCount;
+
+					int currentPosts = 0;
+					progressString.SetBox(2, currentPosts);
+
+					reportProgress();
+
+					//if (jsonFile.StartsWith(config.Value.FileLocation))
+					//	continue;
+
+					var thread = ReadJson<TThread>(jsonFile);
+
+					progressString.SetBox(3, thread.Posts.Count);
+
+					var existingThread = await dbContext.Threads.FirstOrDefaultAsync(x => x.BoardId == boardObject.Id && x.ThreadId == thread.OriginalPost.PostNumber);
+
+					//var lastModifiedTime = thread.Posts.Max(x => x.CreationDateTime).UtcDateTime;
+
+					if (existingThread != null)
+					{
+						//existingThread.IsDeleted = thread.IsDeleted == true;
+						//existingThread.IsArchived = thread.Archived;
+						//existingThread.LastModified = lastModifiedTime;
+						//existingThread.Title = thread.Subject;
+
+						SetThreadInfo(boardObject.Id, thread, ref existingThread, true);
+					}
+					else
+					{
+						SetThreadInfo(boardObject.Id, thread, ref existingThread, false);
+						//existingThread = new DBThread
+						//{
+						//	BoardId = boardObject.Id,
+						//	ThreadId = thread.OriginalPost.PostNumber,
+						//	Title = thread.OriginalPost.Subject,
+						//	IsArchived = thread.Archived,
+						//	IsDeleted = thread.IsDeleted == true,
+						//	LastModified = lastModifiedTime
+						//};
+
+						dbContext.Add(existingThread);
+					}
+
+					var existingPosts = await dbContext.Posts.Where(x => x.BoardId == boardObject.Id && x.ThreadId == thread.OriginalPost.PostNumber).ToArrayAsync();
+
+
+					foreach (var post in thread.Posts)
+					{
+						progressString.SetBox(2, ++currentPosts);
+						
+						var existingPost = existingPosts.FirstOrDefault(x => x.BoardId == boardObject.Id && x.PostId == post.PostNumber);
+
+						if (existingPost != null)
+						{
+							SetPostInfo(boardObject.Id, thread, post, ref existingPost, true);
+
+							//existingPost.Author = post.Name;
+							//existingPost.DateTime = post.CreationDateTime.UtcDateTime;
+							//existingPost.IsDeleted = post.ExtensionIsDeleted == true;
+							//existingPost.ContentHtml = post.Markdown;
+							//existingPost.ContentRaw = post.Message;
+						}
+						else
+						{
+							SetPostInfo(boardObject.Id, thread, post, ref existingPost, false);
+
+							//existingPost = new DBPost
+							//{
+							//	BoardId = boardObject.Id,
+							//	PostId = post.PostNumber,
+							//	ThreadId = thread.OriginalPost.PostNumber,
+							//	ContentHtml = post.Markdown,
+							//	ContentRaw = post.Message,
+							//	Author = post.Name == "Anonymous" ? null : post.Name,
+							//	DateTime = post.CreationDateTime.UtcDateTime,
+							//	IsDeleted = post.ExtensionIsDeleted == true
+							//};
+
+							// This block of logic is to fix a bug with JSON files specifying the same posts multiple times
+							var trackedPost = dbContext.Posts.Local.FirstOrDefault(x => x.BoardId == boardObject.Id && x.PostId == post.PostNumber);
+
+							if (trackedPost != null)
+							{
+								dbContext.Entry(trackedPost).State = EntityState.Detached;
+							}
+
+							dbContext.Add(existingPost);
+							await dbContext.SaveChangesAsync();
+						}
+
+						int index = -1;
+
+						foreach (var file in GetPostFiles(post))
+						{
+							index++;
+
+							string sourceFilename = Path.Combine(subfolder, GetFilesystemSourceFilePath(board, thread, post, file));
+							//string sourceFilename = Path.Combine(subfolder, existingThread.ThreadId.ToString(), file.DirectPath);
+
+							var (newDbFile, _) = await FileImporterTools.UpdateDbFile(sourceFilename);
+
+							var existingDbFile = await dbContext.Files.FirstOrDefaultAsync(x => x.BoardId == boardObject.Id && x.Sha256Hash == newDbFile.Sha256Hash);
+
+							if (existingDbFile == null)
+							{
+								newDbFile.BoardId = boardObject.Id;
+								newDbFile.Extension = sourceFilename.Substring(sourceFilename.LastIndexOf('.') + 1);
+
+								dbContext.Add(newDbFile);
+								await dbContext.SaveChangesAsync();
+
+								existingDbFile = newDbFile;
+
+								var base64Name = Utility.ConvertToBase(existingDbFile.Md5Hash);
+
+								if (string.IsNullOrWhiteSpace(base64Name))
+									Debugger.Break();
+
+								var destinationFilename = Path.Combine(config.Value.FileLocation, board, "image",
+									$"{base64Name}.{existingDbFile.Extension}");
+
+								if (!File.Exists(destinationFilename))
+								{
+									File.Copy(sourceFilename, destinationFilename);
+
+									//sourceFilename = Path.Combine(subfolder, existingPost.ThreadId.ToString(), "thumbs",
+									//	file.DirectThumbPath);
+									sourceFilename = Path.Combine(subfolder, GetFilesystemThumbnailFilePath(board, thread, post, file));
+
+									// this may not always result in .jpg....
+									destinationFilename = Path.Combine(config.Value.FileLocation, board, "thumb",
+										base64Name + ".jpg");
+
+									File.Copy(sourceFilename, destinationFilename);
+								}
+							}
+
+							var existingFileMapping = await dbContext.FileMappings.FirstOrDefaultAsync(x =>
+								x.BoardId == boardObject.Id && x.PostId == post.PostNumber && x.FileId == existingDbFile.Id);
+
+							if (existingFileMapping == null)
+							{
+								SetFileInfo(boardObject.Id, thread, post, file, index, existingDbFile, ref existingFileMapping, false);
+								//existingFileMapping = new DBFileMapping
+								//{
+								//	BoardId = boardObject.Id,
+								//	FileId = existingDbFile.Id,
+								//	PostId = post.PostNumber,
+								//	Index = (byte)index,
+								//	Filename = !file.OriginalName.Contains('.') ? file.OriginalName : file.OriginalName.Remove(file.OriginalName.LastIndexOf('.')),
+								//	IsDeleted = file.IsDeleted == true,
+								//	IsSpoiler = file.ThumbnailUrl.Contains("spoiler")
+								//};
+
+								dbContext.FileMappings.Add(existingFileMapping);
+							}
+							else
+							{
+								SetFileInfo(boardObject.Id, thread, post, file, index, existingDbFile, ref existingFileMapping, true);
+								//existingFileMapping.IsSpoiler = file.ThumbnailUrl.Contains("spoiler");
+							}
+						}
+					}
+
+					await dbContext.SaveChangesAsync();
+					dbContext.DetachAllEntities();
+
+					File.Copy(jsonFile, Path.Combine(config.Value.FileLocation, board, "thread", thread.OriginalPost.PostNumber + ".json"), true);
+				}
+			}
+
+			progressString = "Done";
+			reportProgress();
+		}
+
+		protected abstract void SetThreadInfo(ushort boardId, TThread thread, ref DBThread dbThread, bool exists);
+		protected abstract void SetPostInfo(ushort boardId, TThread thread, TPost post, ref DBPost dbPost, bool exists);
+		protected abstract void SetFileInfo(ushort boardId, TThread thread, TPost post, TPostFile postFile, int index, DBFile dbFile, ref DBFileMapping dbFileMapping, bool exists);
+
+		protected abstract IEnumerable<TPostFile> GetPostFiles(TPost post);
+
+		protected abstract string GetFilesystemSourceFilePath(string board, TThread thread, TPost post, TPostFile postFile);
+		protected abstract string GetFilesystemThumbnailFilePath(string board, TThread thread, TPost post, TPostFile postFile);
+
+	}
+
+	public static class FileImporterTools
+	{
+		internal static async Task<(DBFile file, bool md5Changed)> UpdateDbFile(string filename, DBFile file = null)
+		{
+			bool newFile = file == null;
+
+			file ??= new DBFile();
+
+			//var json = await RunMagickCommandAsync("magick" $"convert \"{filename}\" json:");
+
+			//file.ImageWidth = json["image"]["geometry"].Value<ushort>("width");
+			//file.ImageHeight = json["image"]["geometry"].Value<ushort>("height");
+
+			try
+			{
+				var result = await RunJsonCommandAsync("ffprobe", $"-v quiet -hide_banner -show_streams -print_format json \"{filename}\"");
+
+				file.ImageWidth = result["streams"][0].Value<ushort>("width");
+				file.ImageHeight = result["streams"][0].Value<ushort>("height");
+			}
+			catch (Exception ex) when (ex.Message.Contains("magick"))
+			{
+				file.ImageWidth = null;
+				file.ImageHeight = null;
+			}
+
+			using var md5 = MD5.Create();
+			using var sha1 = SHA1.Create();
+			using var sha256 = SHA256.Create();
+
+			await using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+			long fileSize = fs.Length;
+
+			await using var readBuffer = new RentedMemoryStream((int)fileSize);
+
+			await fs.CopyToAsync(readBuffer);
+
+			var oldMd5Hash = file.Md5Hash;
+
+			readBuffer.Position = 0;
+			file.Md5Hash = md5.ComputeHash(readBuffer);
+
+			readBuffer.Position = 0;
+			file.Sha1Hash = sha1.ComputeHash(readBuffer);
+
+			readBuffer.Position = 0;
+			file.Sha256Hash = sha256.ComputeHash(readBuffer);
+
+			file.Size = (uint)fileSize;
+
+			bool md5HashChanged = !oldMd5Hash.ByteArrayEquals(file.Md5Hash);
+
+			if (md5HashChanged && !newFile)
+			{
+				file.OriginalMd5Hash = oldMd5Hash;
+
+				JArray array;
+
+				if (file.Md5ConflictHistory == null)
+					array = new JArray();
+				else
+					array = JArray.Parse(file.Md5ConflictHistory);
+				
+				array.Add(new Md5Conflict(oldMd5Hash, file.Md5Hash));
+
+				file.Md5ConflictHistory = array.ToString(Formatting.None);
+			}
+
+			return (file, md5HashChanged);
+		}
+
+		internal static async Task<JObject> RunJsonCommandAsync(string executable, string arguments)
+		{
+			using var process = new Process
+			{
+				StartInfo = new ProcessStartInfo(executable, arguments)
+				{
+					UseShellExecute = false,
+					RedirectStandardError = true,
+					RedirectStandardOutput = true
+				}
+			};
+
+			var tcs = new TaskCompletionSource<object>();
+
+			process.Exited += (sender, e) => tcs.SetResult(null);
+
+			process.EnableRaisingEvents = true;
+
+			process.Start();
+
+			var errorTask = process.StandardError.ReadToEndAsync();
+
+			using var jsonReader = new JsonTextReader(process.StandardOutput);
+
+
+			//_ = process.StandardError.BaseStream.CopyToAsync(Stream.Null);
+
+			var result = await JObject.LoadAsync(jsonReader);
+			var error = await errorTask;
+
+			await tcs.Task;
+
+			if (process.ExitCode > 0)
+			{
+				throw new Exception($"magick returned {process.ExitCode}: {error}");
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Hayden.WebServer/Logic/Importer/FoolFuukaImporter.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Hayden.Models;
+using Hayden.WebServer.DB;
+
+namespace Hayden.WebServer.Logic.Importer
+{
+    public class FoolFuukaImporter : BaseImporter<FoolFuukaThread, FoolFuukaPost, FoolFuukaPostMedia>
+    {
+		protected override void SetThreadInfo(ushort boardId, FoolFuukaThread thread, ref DBThread dbThread, bool exists)
+		{
+			var lastModifiedTime = Utility.ConvertGMTTimestamp(thread.Posts.Max(x => x.UnixTimestamp)).UtcDateTime;
+
+			if (exists)
+			{
+				dbThread.IsDeleted = thread.IsDeleted == true;
+				dbThread.IsArchived = thread.Archived;
+				dbThread.LastModified = lastModifiedTime;
+				dbThread.Title = thread.OriginalPost.Title;
+			}
+			else
+			{
+				dbThread = new DBThread
+				{
+					BoardId = boardId,
+					ThreadId = thread.OriginalPost.PostNumber,
+					Title = thread.OriginalPost.Title,
+					IsArchived = thread.Archived,
+					IsDeleted = thread.IsDeleted == true,
+					LastModified = lastModifiedTime
+				};
+			}
+		}
+
+		protected override void SetPostInfo(ushort boardId, FoolFuukaThread thread, FoolFuukaPost post, ref DBPost dbPost, bool exists)
+		{
+			if (exists)
+			{
+				dbPost.Author = post.Author == "Anonymous" ? null : post.Author;
+				dbPost.Email = post.Email;
+				dbPost.Tripcode = post.Tripcode;
+				dbPost.DateTime = Utility.ConvertGMTTimestamp(post.UnixTimestamp).UtcDateTime;
+				dbPost.IsDeleted = post.ExtensionIsDeleted == true;
+				dbPost.ContentRaw = post.SanitizedComment;
+			}
+			else
+			{
+				dbPost = new DBPost
+				{
+					BoardId = boardId,
+					PostId = post.PostNumber,
+					ThreadId = thread.OriginalPost.PostNumber,
+					ContentHtml = null,
+					ContentRaw = thread.OriginalPost.SanitizedComment,
+					Author = post.Author == "Anonymous" ? null : post.Author,
+					Email = post.Email,
+					Tripcode = post.Tripcode,
+					DateTime = Utility.ConvertGMTTimestamp(post.UnixTimestamp).UtcDateTime,
+					IsDeleted = post.ExtensionIsDeleted == true
+				};
+			}
+		}
+
+		protected override void SetFileInfo(ushort boardId, FoolFuukaThread thread, FoolFuukaPost post, FoolFuukaPostMedia postFile, int index, DBFile dbFile, ref DBFileMapping dbFileMapping, bool exists)
+		{
+			if (!exists)
+			{
+				dbFileMapping = new DBFileMapping
+				{
+					BoardId = boardId,
+					FileId = dbFile.Id,
+					PostId = post.PostNumber,
+					Index = (byte)index,
+					Filename = !postFile.OriginalFilename.Contains('.') ? postFile.OriginalFilename : postFile.OriginalFilename.Remove(postFile.OriginalFilename.LastIndexOf('.')),
+					IsDeleted = false,
+					IsSpoiler = postFile.ThumbnailUrl.Contains("spoiler")
+				};
+			}
+			else
+			{
+				dbFileMapping.IsSpoiler = postFile.ThumbnailUrl.Contains("spoiler");
+			}
+		}
+
+		protected override IEnumerable<FoolFuukaPostMedia> GetPostFiles(FoolFuukaPost post)
+			=> post.Media == null ? Enumerable.Empty<FoolFuukaPostMedia>() : Enumerable.Repeat(post.Media, 1);
+
+		protected override string GetFilesystemSourceFilePath(string board, FoolFuukaThread thread, FoolFuukaPost post, FoolFuukaPostMedia postFile)
+		{
+			return Path.Combine(thread.OriginalPost.PostNumber.ToString(), postFile.TimestampedFilename);
+		}
+
+		protected override string GetFilesystemThumbnailFilePath(string board, FoolFuukaThread thread, FoolFuukaPost post, FoolFuukaPostMedia postFile)
+		{
+			return Path.Combine(thread.OriginalPost.PostNumber.ToString(), "thumbs", postFile.TimestampedThumbFilename);
+		}
+	}
+}
