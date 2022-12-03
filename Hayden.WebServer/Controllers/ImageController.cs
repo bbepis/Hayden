39a9f0dcad56446f0912ef7c7442686b5ada2f1e
Bepis <36346617+bbepis@users.noreply.github.com>
2022-12-04 00:50:22 +1100

    feat(Server): add endpoint to retrieve file by id

--- Hayden.WebServer/Controllers/ImageController.cs
+++ Hayden.WebServer/Controllers/ImageController.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Hayden.Consumers.HaydenMysql.DB;
+using Hayden.WebServer.View;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace Hayden.WebServer.Controllers
@@ -28,6 +31,35 @@ namespace Hayden.WebServer.Controllers
 				return NotFound();
 
 			return File(System.IO.File.OpenRead(fullPath), path.EndsWith("png") ? "image/png" : "image/jpeg");
+		}
+
+		/// <summary>
+		/// Serves an image by ID.
+		/// </summary>
+		[HttpGet]
+		[Route("id/{id}")]
+		public async Task<IActionResult> ImageId(uint id, [FromServices] HaydenDbContext dbContext)
+		{
+			var file = await dbContext.Files.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+			if (file == null)
+				return NotFound();
+
+			var board = await dbContext.Boards.FindAsync(file.BoardId);
+
+			if (Config.Value.ImagePrefix != null)
+			{
+				var urls = PostPartialViewModel.GenerateUrls(file, board.ShortName, Config.Value);
+				return Redirect(urls.imageUrl);
+			}
+
+			string fullPath = Common.CalculateFilename(Config.Value.FileLocation, board.ShortName,
+				Common.MediaType.Image, file.Sha256Hash, file.Extension);
+
+			if (!System.IO.File.Exists(fullPath))
+				return NotFound();
+
+			return File(System.IO.File.OpenRead(fullPath), fullPath.EndsWith("png") ? "image/png" : "image/jpeg");
 		}
 	}
 }
